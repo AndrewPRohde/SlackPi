9144d0c013b4c288f690654929944ff736e9b53a
Andrew Rohde <andrewprohde@gmail.com>
2022-07-04 14:29:12 -0500

    added dependencies

--- PiHandler.py
+++ PiHandler.py
@@ -1,9 +1,13 @@
 from time import sleep
 from datetime import datetime
+from signal import signal, SIGTERM, SIGHUP, pause
+from rpi_lcd import LCD
 
 #LCD Reference
 #https://www.electroniclinic.com/raspberry-pi-16x2-lcd-i2c-interfacing-and-python-programming/
 #https://github.com/sterlingbeason/LCD-1602-I2C
 
-lcd_columns = 16
-lcd_rows = 2
+lcd = LCD()
+def safe_exit(signum, frame):
+    exit(1)
+
